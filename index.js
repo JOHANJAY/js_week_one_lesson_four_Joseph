6b218aed282b7fb82c062e0df5ab237b660d9616
John Joseph <joseph.john33@yahoo.com>
2024-09-09 02:15:52 +0100

    lessonFour submission

---
+++ index.js
@@ -0,0 +1,74 @@
+//Task One
+/* function greet(params) {
+   return console.log('Hello');  
+}
+
+greet()
+ */
+//Task Two
+/* function greet(name) {
+   return console.log(`Hello ${name}`);  
+}
+
+greet('jay') */
+
+//Task Three
+/* function muliply(a, b) {
+  return console.log(a * b);
+}
+muliply(4, 10);
+ */
+
+//Task Four
+/* function greet(name = "guest") {
+  return console.log(`Hello ${name}`);
+}
+
+greet("jay");
+greet(); */
+
+//Task Five
+/* const add = function () {
+  return console.log(2 + 3);
+};
+
+add(); */
+
+//Task Six
+/* const greet = (name) => {
+    return console.log(`Hello ${name}`)
+}
+greet("Emmanuel")
+ */
+
+//Task Seven
+/* const muliply = () => {
+    let num = 5
+}
+console.log(num); */
+
+/* let num = 3;
+const add = () => {
+  return num + 2;
+};
+
+console.log(add()); */
+
+//Task Eight
+/* function outerFunction(name) {
+  let greeting = "Hello";
+
+  return function () {
+    console.log(`${greeting}, ${name}!`);
+  };
+}
+
+const greetPerson = outerFunction("Joseph");
+
+greetPerson(); */
+
+//Task Nine
+/* (() => {
+  console.log(2 + 3);
+})();
+ */
